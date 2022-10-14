a5884187b24a914a80d9ff9fdff76e277e25d465
Glitch <bright.glitch@gmail.com>
2022-10-13 22:03:12 -0300

    added total staked skeleton

--- frontend/components/pages/stake/blocks/Staked/Staked.tsx
+++ frontend/components/pages/stake/blocks/Staked/Staked.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import style from './Staked.module.scss'
+import Image from 'next/image'
 
 const Staked = () => {
   return (
-    <div>Staked</div>
+    <>
+      <div className={style.title} >Total Staked</div>
+
+      <hr className={style.hr} />
+
+      <div style={{ display:'flex', justifyContent:'space-between', alignItems:'center' }} >
+
+      <div className={style.balance} >635418941 <span style={{fontSize:'30px', color:'#BB000E'}} >FTR</span></div>
+
+      <div className={style.img} >
+        <Image src='/images/stake.png' width={100} height={100} />
+      </div>
+      </div>
+    </>
   )
 }
 
